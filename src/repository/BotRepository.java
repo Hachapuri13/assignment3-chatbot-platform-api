1a4d7bc3773d4ab53745995c95edb29d41ce0194
Hachapuri13 <k.potato4ver@gmail.com>
2026-01-22 06:30:22 +0500

    feat(repo): add CRUD operations using secure PreparedStatement

---
+++ src/repository/BotRepository.java
@@ -0,0 +1,60 @@
+package repository;
+
+import model.Bot;
+import utils.DatabaseConnection;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BotRepository {
+
+    public void create(Bot bot) throws SQLException {
+        String sql = "INSERT INTO bots (name, greeting, definition, token_limit) VALUES (?, ?, ?, ?)";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, bot.getName());
+            pstmt.setString(2, bot.getGreeting());
+            pstmt.setString(3, bot.getDefinition());
+            pstmt.setInt(4, bot.getTokenLimit());
+            pstmt.executeUpdate();
+        }
+    }
+
+    public List<Bot> getAll() throws SQLException {
+        List<Bot> bots = new ArrayList<>();
+        String sql = "SELECT * FROM bots";
+        try (Connection conn = DatabaseConnection.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                bots.add(new Bot(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("greeting"),
+                        rs.getString("definition"),
+                        rs.getInt("token_limit")
+                ));
+            }
+        }
+        return bots;
+    }
+
+    public Bot getById(int id) throws SQLException {
+        String sql = "SELECT * FROM bots WHERE id = ?";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, id);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return new Bot(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("greeting"),
+                        rs.getString("definition"),
+                        rs.getInt("token_limit")
+                );
+            }
+        }
+        return null;
+    }
+}
